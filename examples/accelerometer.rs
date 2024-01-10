ed70531d07a3e8b6f56e58f09cf079d2c9c3ce83
Sebastian Imlay <sebastian.imlay@gmail.com>
2024-01-09 23:37:38 -0500

    Simple example

---
+++ examples/accelerometer.rs
@@ -0,0 +1,30 @@
+use coremotion::{
+    CMMotionManager,
+    ICMMotionManager, ICMAccelerometerData,
+    INSObject,
+};
+
+use oslog::OsLogger;
+use log::LevelFilter;
+fn main() {
+    OsLogger::new("com.simlay.net")
+        .level_filter(LevelFilter::Debug)
+        .category_level_filter("Settings", LevelFilter::Trace)
+        .init()
+        .unwrap();
+
+    let manager = CMMotionManager::alloc();
+    unsafe {
+        manager.init();
+        let available = manager.isAccelerometerAvailable();
+        log::info!("Accelerometer {available}");
+        //manager.setAccelerometerUpdateInterval_(10.0);
+        manager.startAccelerometerUpdates();
+        for i in 1..1000 {
+            let data = manager.accelerometerData();
+            let acceleration = data.acceleration();
+            log::info!("Sample {i} - {acceleration:?}");
+            std::thread::sleep(std::time::Duration::from_millis(10));
+        }
+    }
+}
